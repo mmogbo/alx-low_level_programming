13092e8b832a3aa3b4857131a3040bfe2680f5da
Mmogbo Alex <mmogbo@outlook.com>
2023-08-14 07:55:21 -0700

    completed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of app meant to print the file name
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
