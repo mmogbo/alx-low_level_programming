a42d95251cbcdad6d27a9a56d1700237f713b20a
Mmogbo Alex <mmogbo@outlook.com>
2023-08-14 08:46:15 -0700

    corrected

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,8 +3,8 @@
 #include "dog.h"
 
 /**
- * print_dog - prints a struct for dog
- * @d: struct dog to be printed
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
  */
 void print_dog(struct dog *d)
 {
